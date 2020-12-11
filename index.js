27a4babd8ef7eb1591a709c7a804fc65a1fe27ee
Prashant Dubey <74289917+DubeyTech@users.noreply.github.com>
2020-12-11 06:51:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', event => {
+    rollTheDice();
+});
+
+function rollTheDice() {
+    //--------------------     Dice 1
+    var randomNumber1 = Math.round((Math.random() * 5) + 1);
+    var randomImg1 = "images/dice" + randomNumber1 + ".png";
+    var img1 = document.querySelectorAll("img")[0];
+    img1.setAttribute("src", randomImg1);
+
+    //-------------------     Dice 2
+    var randomNumber2 =  Math.round((Math.random() * 5) + 1);
+    var randomImg2 = "images/dice" + randomNumber2 + ".png";
+    var img2 = document.querySelectorAll("img")[1];
+    img2.setAttribute("src", randomImg2);
+
+    // --------------------    Result
+    var title = document.querySelector("h1");
+
+    if (randomNumber1 < randomNumber2) {
+        title.innerText = "Player 2 Wins !!! 🏆";
+    }
+    else if (randomNumber1 > randomNumber2) {
+        title.innerText = "🏆 Player 1 Wins !!! ";
+    }
+    else {
+        title.innerHTML = " !!! &nbsp&nbsp DRAW &nbsp&nbsp !!!";
+    }
+}
