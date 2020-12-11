971c6dff7956c772847a23473af1be59ac8750f5
Prashant Dubey <74289917+DubeyTech@users.noreply.github.com>
2020-12-11 07:18:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 
+var player1 = prompt("Enter player 1 name.");
+var player2 = prompt("Enter player 2 name.");
+
+document.querySelectorAll('.dice p')[0].innerText = player1;
+document.querySelectorAll('.dice p')[1].innerText = player2;
+
+
 const button = document.querySelector('button');
 
 button.addEventListener('click', event => {
@@ -8,13 +15,13 @@ button.addEventListener('click', event => {
 function rollTheDice() {
     //--------------------     Dice 1
     var randomNumber1 = Math.round((Math.random() * 5) + 1);
-    var randomImg1 = "dice" + randomNumber1 + ".png";
+    var randomImg1 = "images/dice" + randomNumber1 + ".png";
     var img1 = document.querySelectorAll("img")[0];
     img1.setAttribute("src", randomImg1);
 
     //-------------------     Dice 2
     var randomNumber2 =  Math.round((Math.random() * 5) + 1);
-    var randomImg2 = "dice" + randomNumber2 + ".png";
+    var randomImg2 = "images/dice" + randomNumber2 + ".png";
     var img2 = document.querySelectorAll("img")[1];
     img2.setAttribute("src", randomImg2);
 
@@ -22,10 +29,10 @@ function rollTheDice() {
     var title = document.querySelector("h1");
 
     if (randomNumber1 < randomNumber2) {
-        title.innerText = "Player 2 Wins ! 🏆";
+        title.innerText = player2 + " Wins ! 🏆";
     }
     else if (randomNumber1 > randomNumber2) {
-        title.innerText = "🏆 Player 1 Wins ! ";
+        title.innerText = "🏆 " + player1 + " Wins ! ";
     }
     else {
         title.innerHTML = " !!! &nbsp&nbsp DRAW &nbsp&nbsp !!!";
